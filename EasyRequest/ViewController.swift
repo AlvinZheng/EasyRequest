93b40ac394f36b1f5bd84155948100626c37f4ba
qingzhou.zheng <qingzhou.zheng@ck-telecom.com>
2017-10-12 15:07:34 +0800

    Initial Commit

---
+++ EasyRequest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyRequest
+//
+//  Created by alvin zheng on 17/10/12.
+//  Copyright © 2017年 alvin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
